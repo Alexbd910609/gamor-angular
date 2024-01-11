7089258aa95e4bc1705daf9a50adfc003475690d
AlexBritodelasCuevas <abritodelascuevas@bankconciliation.com>
2024-01-11 15:02:43 -0500

    Games categories makeup

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {BodyComponent} from "@components/body/body.component";
+import {CategoriesComponent} from "@components/categories/categories.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    BodyComponent
+    BodyComponent,
+    CategoriesComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
